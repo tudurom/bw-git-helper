41bc34758acf1d5779bf758dd40c1100dc5d9a9e
Tudor Roman <tudor@tudorr.ro>
2022-01-22 15:11:45 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -2,51 +2,37 @@
   description = "Bitwarden Git credential helper";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/release-20.09";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: let
-    lib = nixpkgs.lib;
-
-    systems = [
-      "x86_64-linux"
-      "i686-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "armv6l-linux"
-      "armv7l-linux"
-    ];
-
-    forAllSystems = f: lib.genAttrs systems (system: f system);
-  in
-    {
-      defaultPackage = forAllSystems (system:
-        let
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages.bw-git-helper = let
           spdx = lic: lic // {
             url = "https://spdx.org/licenses/${lic.spdxId}.html";
           };
+        in pkgs.buildGoModule rec {
+          pname = "bw-git-helper";
+          version = "unstable";
 
-          pkgs = import nixpkgs {
-            inherit system;
+          src = ./.;
+
+          vendorSha256 = "1x7iwd4ndcvcwyx8cxhkcwn5kwwf8nam47ln743wnvcrxi7q2604";
+          buildInputs = with pkgs; [ bitwarden-cli ];
+
+          meta = with pkgs.lib; {
+            description = "A git credential helper using BitWarden as a backend";
+            homepage = "https://github.com/tudurom/bw-git-helper";
+            license = spdx { spdxId = "EUPL-1.2"; };
+            maintainers = [ maintainers.tudor ];
+            platforms = platforms.all;
           };
-        in
-          pkgs.buildGoModule rec {
-            pname = "bw-git-helper";
-            version = "unstable";
-
-            src = ./.;
-
-            vendorSha256 = "1x7iwd4ndcvcwyx8cxhkcwn5kwwf8nam47ln743wnvcrxi7q2604";
-            buildInputs = with pkgs; [ bitwarden-cli ];
-
-            meta = with pkgs.stdenv.lib; {
-              description = "A git credential helper using BitWarden as a backend";
-              homepage = "https://github.com/tudurom/bw-git-helper";
-              license = spdx { spdxId = "EUPL-1.2"; };
-              maintainers = [ maintainers.tudor ];
-              platforms = platforms.all;
-            };
-          }
-      );
-    };
+        };
+        defaultPackage = packages.bw-git-helper;
+        apps.bw-git-helper = utils.lib.mkApp { drv = packages.bw-git-helper; };
+        defaultApp = apps.bw-git-helper;
+    });
 }
